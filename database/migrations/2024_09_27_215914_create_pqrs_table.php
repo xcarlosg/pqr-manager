816868fb0b8d8fc60b8d77aab324941949a7a6b6
xcarlosg <carlosmgomzalez@gmail.com>
2024-09-27 17:43:14 -0500

    feat: add migration for the pqrs table

---
+++ database/migrations/2024_09_27_215914_create_pqrs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pqrs', function (Blueprint $table) {
+            $table->id('pqr_id');
+            $table->date('pqr_date');
+            $table->string('pqr_type');
+            $table->string('pqr_sendnotification');
+            $table->text('pqr_cause');
+            $table->text('pqr_observation');
+            $table->string('pqr_evidence')->nullable();
+            $table->foreignId('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pqrs');
+    }
+};
